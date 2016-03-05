3c250d543211c9417f037b8fbfa54486c70c627d
tomas-kasalicky <kasalicky.tomas@gmail.com>
2016-03-05 23:17:31 +0100

    Added and removed products detection

--- watsonthread.py
+++ watsonthread.py
@@ -23,58 +23,54 @@ class watsonThread(threading.Thread):
 
         if("scores" in tags["images"][0]):
             tag = tags["images"][0]["scores"][0]["name"]
-        else:
-            tag = "empty"
-
-        pusher = Pusher(
-            app_id='185391',
-            key='99c8766f736643bbdfa2',
-            secret='68a32b237af4cb110394',
-            cluster='eu',
-            ssl=True
-        )
-
-        print("received tag: ")
-        print(tag)
-
-        if(tag != watsonThread.lastaction):
-            if(tag == "empty"):
-                # do nothing
-                watsonThread.lastaction = ""
-            elif(tag == "hand_empty"):
-                # not used (yet)
-                watsonThread.lastaction = ""
-            elif(tag == "inside_fridge"):
-                if(watsonThread.lastaction != "empty" and watsonThread.lastaction != "hand_empty"):
-                    # ADDING PRODUCT INTO FRIDGE
-
-                    with self.app.app_context():
-                        prod = Product.query.filter_by(tag=tag).first()
-                        prod.add(1)
 
 
-                    # notify UI
-                    sendNotification()
-                    notification = {'tag': watsonThread.lastaction, 'filename': self.filename, 'name':'nothing' }
-                    pusher.trigger('messages', 'new_product', notification)
+            pusher = Pusher(
+                app_id='185391',
+                key='99c8766f736643bbdfa2',
+                secret='68a32b237af4cb110394',
+                cluster='eu',
+                ssl=True
+            )
 
-                watsonThread.lastaction = "inside_fridge"
-            else:
-                # PRODUCT
-                if(watsonThread.lastaction == "inside_fridge"):
-                    # PRODUCT TAKEN FROM FRIDGE
+            print("received tag: ")
+            print(tag)
 
-                    with self.app.app_context():
-                        prod = Product.query.filter_by(tag=tag).first()
-                        prod.remove(1)
+            if(tag != watsonThread.lastaction):
+                if(tag == "empty"):
+                    # do nothing
+                    watsonThread.lastaction = ""
+                elif(tag == "inside_fridge" or tag == "hand_empty"):
+                    if(watsonThread.lastaction != "empty" and watsonThread.lastaction != "hand_empty"):
+                        # ADDING PRODUCT INTO FRIDGE
+
+                        with self.app.app_context():
+                            prod = Product.query.filter_by(tag=tag).first()
+                            prod.add(1)
 
 
-                    # notify UI
-                    sendNotification()
-                    notification = {'tag': tag, 'filename': self.filename, 'name':'nothing' }
-                    pusher.trigger('messages', 'new_product', notification)
+                        # notify UI
+                        sendNotification()
+                        notification = {'tag': watsonThread.lastaction, 'filename': self.filename, 'name':'nothing' }
+                        pusher.trigger('messages', 'new_product', notification)
 
-                watsonThread.lastaction = tag
+                    watsonThread.lastaction = "inside_fridge"
+                else:
+                    # PRODUCT
+                    if(watsonThread.lastaction == "inside_fridge"):
+                        # PRODUCT TAKEN FROM FRIDGE
+
+                        with self.app.app_context():
+                            prod = Product.query.filter_by(tag=tag).first()
+                            prod.remove(1)
+
+
+                        # notify UI
+                        sendNotification()
+                        notification = {'tag': tag, 'filename': self.filename, 'name':'nothing' }
+                        pusher.trigger('messages', 'new_product', notification)
+
+                    watsonThread.lastaction = tag
 
 
 
