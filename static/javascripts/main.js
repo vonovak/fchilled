0e163f25759af4f7d678a50b94c6e52b5a54f372
Vojtech Novak <vonovak@gmail.com>
2016-03-05 18:04:40 +0100

    working toward push notif

--- static/javascripts/main.js
+++ static/javascripts/main.js
@@ -5,7 +5,19 @@ if ('serviceWorker' in navigator) {
     console.log('Service Worker is supported');
     navigator.serviceWorker.register('/static/javascripts/sw.js').then(function (reg) {
         console.log(':^)', reg);
-        // TODO
+        if ('serviceWorker' in navigator) {
+            console.log('Service Worker is supported');
+            navigator.serviceWorker.register('/static/javascripts/sw.js').then(function (reg) {
+                console.log(':^)', reg);
+                reg.pushManager.subscribe({
+                    userVisibleOnly: true
+                }).then(function (sub) {
+                    console.log('endpoint:', sub.endpoint);
+                });
+            }).catch(function (error) {
+                console.log(':^(', error);
+            });
+        }
     }).catch(function (err) {
         console.log(':^(', err);
     });
