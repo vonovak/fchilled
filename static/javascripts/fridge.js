f082b5ebcd282a5ab7b22703b5ea4e16c480f21b
Michal <michal.kral@live.com>
2016-03-05 22:18:12 +0100

    react part

--- static/javascripts/fridge.js
+++ static/javascripts/fridge.js
@@ -1,10 +1,61 @@
-var Hello = React.createClass({
-  render: function() {
-    return <div>Hello {this.props.name}</div>;
+
+var Form = React.createClass({
+  render: function () {
+    return (
+        <div className="form">
+          <a href="#">Yes!</a> | <a href="">No</a>
+        </div>
+    );
   }
-});
+})
+
+var Item = React.createClass({
+  render: function() {
+    var data = this.props.data;
+
+    if (!data.tag) {
+      return <p>Insert items!</p>;
+    }
+    return (
+        <div className="product">
+          {data.tag}
+          <img src={'http://fchilled.eu-gb.mybluemix.net/uploads/' + data.filename + '.jpg'} />
+          <Form />
+        </div>
+    );
+  }
+})
+
+var Fridge = React.createClass({
+  getInitialState: function() {
+    return {data: {tag:'', name:'', filename:''}};
+  },
+
+  componentWillMount: function() {
+    this.pusher = new Pusher('99c8766f736643bbdfa2', {
+      cluster: 'eu',
+      encrypted: true
+    });
+    this.productShelf = this.pusher.subscribe('messages');
+  },
+
+  componentDidMount: function() {
+    this.productShelf.bind('new_product', function(product){
+      this.setState({data: {tag: product.tag, name: product.name, filename: product.filename}})
+    }, this);
+  },
+
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Fridge:</h1>
+        <Item data={this.state.data} />
+      </div>
+    );
+  }
+})
 
 ReactDOM.render(
-  <Hello name="World" />,
+  <Fridge />,
   document.getElementById('content')
 );
